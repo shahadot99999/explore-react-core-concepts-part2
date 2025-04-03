cf2e49bff23417662b871969a4a6cb92d470d22b
shahadot99999 <shahadot99999@gmail.com>
2025-04-03 10:54:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  function handleClick(){
+    alert('button clicked');
+  }
+
+  const handleClick2 =()=>{
+    alert('button 2 clicked');
+  }
+
+  const addToFive =(num)=>{
+    alert(num + 5);
+  }
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={()=>{alert('third clicked')}}>third</button>
+      <button onClick={()=>addToFive(3)}>Four</button>
+     
+    </>
+  )
+}
+
+export default App
