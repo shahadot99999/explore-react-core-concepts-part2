d61a19896442b8e8b87857ca3f3193428fd26296
shahadot99999 <shahadot99999@gmail.com>
2025-04-11 23:50:06 +0600

    spilitng medium problem solve

--- src/Splitting.jsx
+++ src/Splitting.jsx
@@ -1,5 +1,7 @@
 //import React from 'react';
 
+import { useState } from "react";
+
 const people = [
     { id: 1, name: 'Marie Curie', profession: 'chemist' },
     { id: 2, name: 'Albert Einstein', profession: 'physicist' },
@@ -9,23 +11,55 @@ const people = [
 
 const Splitting = () => {
 
+    const [showChemists, setShowChemists] = useState(true);
+
      // Filter chemists and non-chemists
   const chemists = people.filter(person => person.profession === 'chemist');
   const others = people.filter(person => person.profession !== 'chemist');
 
     return (
-        <div>
-      <h2>Chemists</h2>
-      <ul>
-        {chemists.map(person => (
-          <li key={person.id}>{person.name}</li>
-        ))}
-      </ul>
+    //     <div>
+    //   <h2>Chemists</h2>
+    //   <ul>
+    //     {chemists.map(person => (
+    //       <li key={person.id}>{person.name}</li>
+    //     ))}
+    //   </ul>
+
+    //   <h2>Everyone Else</h2>
+    //   <ul>
+    //     {others.map(person => (
+    //       <li key={person.id}>{person.name}</li>
+    //     ))}
+    //   </ul>
+    // </div>
+
+
+
+    <div>
+      <button onClick={() => setShowChemists(!showChemists)}>
+        {showChemists ? 'Hide Chemists' : 'Show Chemists'}
+      </button>
+
+      {showChemists && (
+        <>
+          <h2>Chemists</h2>
+          <ul>
+            {chemists.map(person => (
+              <li key={person.id}>
+                {person.name} - {person.profession}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
 
       <h2>Everyone Else</h2>
       <ul>
         {others.map(person => (
-          <li key={person.id}>{person.name}</li>
+          <li key={person.id}>
+            {person.name} - {person.profession}
+          </li>
         ))}
       </ul>
     </div>
